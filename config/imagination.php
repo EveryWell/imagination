bce21307ff90d6ba1ec4faabfb658c396593a1da
EveryWell <aneverywell@gmail.com>
2017-08-21 15:20:22 +0200

    updated config file

--- config/imagination.php
+++ config/imagination.php
@@ -4,67 +4,10 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Package Connection
+    | Base path
     |--------------------------------------------------------------------------
-    |
-    | You can set a different database connection for this package. It will set
-    | new connection for models Role and Permission. When this option is null,
-    | it will connect to the main database, which is set up in database.php
     |
     */
 
-    'connection' => null,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Slug Separator
-    |--------------------------------------------------------------------------
-    |
-    | Here you can change the slug separator. This is very important in matter
-    | of magic method __call() and also a `Slugable` trait. The default value
-    | is a dot.
-    |
-    */
-
-    'separator' => '.',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Models
-    |--------------------------------------------------------------------------
-    |
-    | If you want, you can replace default models from this package by models
-    | you created. Have a look at `Bican\Roles\Models\Role` model and
-    | `Bican\Roles\Models\Permission` model.
-    |
-    */
-
-    'models' => [
-        'role' => Bican\Roles\Models\Role::class,
-        'permission' => Bican\Roles\Models\Permission::class,
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Roles, Permissions and Allowed "Pretend"
-    |--------------------------------------------------------------------------
-    |
-    | You can pretend or simulate package behavior no matter what is in your
-    | database. It is really useful when you are testing you application.
-    | Set up what will methods is(), can() and allowed() return.
-    |
-    */
-
-    'pretend' => [
-
-        'enabled' => false,
-
-        'options' => [
-            'isRole' => true,
-            'may' => true,
-            'allowed' => true,
-        ],
-
-    ],
-
+    'base_path' => env('IMAGES_BASE_PATH', '')
 ];
